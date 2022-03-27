0e682849ca9a87f99d106b8b9f59704f6622f154
Yuheng Ding <d.yuheng@wustl.edu>
2022-03-26 23:30:37 -0500

    Set up TabView

--- TennisBuddy/ContentView.swift
+++ TennisBuddy/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 0
+    
+    let scrollEdgeAppearance =  UITabBarAppearance()
+    
+    init() {
+        UITabBar.appearance().backgroundColor = .lightGray
+        UITabBar.appearance().scrollEdgeAppearance = scrollEdgeAppearance
+    }
+    
+    // TODO: resize icons
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            Text("Games")
+                .font(.system(size: 30))
+                .tabItem {
+                    Image(systemName: "calendar.circle.fill")
+                }
+                .tag(0)
+            
+            Text("Ranking")
+                .font(.system(size: 30))
+                .tabItem {
+                    Image(systemName: "line.3.horizontal.circle.fill") // placeholder until a better icon is found
+                }
+                .tag(1)
+            
+            Text("Message")
+                .font(.system(size: 30))
+                .tabItem {
+                    Image(systemName: "message.circle.fill")
+                }
+                .tag(2)
+            
+            Text("Profile")
+                .font(.system(size: 30))
+                .tabItem {
+                    Image(systemName: "person.circle.fill")
+                }
+                .tag(3)
+        }
     }
 }
 
