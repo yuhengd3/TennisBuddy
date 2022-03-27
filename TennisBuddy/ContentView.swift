fac0d415d510074eea213545234838244b78006b
Yuheng Ding <d.yuheng@wustl.edu>
2022-03-26 22:36:59 -0500

    Initial Commit

---
+++ TennisBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TennisBuddy
+//
+//  Created by Yuheng Ding on 2022/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TennisBuddy/TennisBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisBuddyApp.swift
+//  TennisBuddy
+//
+//  Created by Yuheng Ding on 2022/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TennisBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
